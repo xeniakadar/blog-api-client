a81a82e2796bc43154d9aaec97ada31ed8363d00
xeniakadar <xenia.kadar@gmail.com>
2023-08-27 10:29:53 +0200

    finished create post functionality

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Listbox } from '@headlessui/react';
 
 export default function PostCreate() {
   //
   //get topics
-  const [topics, setTopics] = useState("");
+  const [topics, setTopics] = useState([]);
   const [selectedTopic, setSelectedTopic] = useState(topics[0]);
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchTopics() {
@@ -17,6 +20,7 @@ export default function PostCreate() {
 
         if (response.ok) {
           setTopics(data);
+          setSelectedTopic(data[0]?.title || null)
         } else {
           console.error("failed to fetch topics", data);
         }
@@ -28,34 +32,33 @@ export default function PostCreate() {
     fetchTopics();
   }, []);
 
-  useEffect(() => {
-    async function createBlogpost(e) {
-      e.preventDefault();
-      try {
-        const response = await fetch("https://blog-api-production-c42d.up.railway.app/api/blogposts", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem("token")}`
-          },
-          body: JSON.stringify({
-            "title": title,
-            "text": text,
-            "topic": selectedTopic,
-          })
-        });
-        if (response.ok) {
-          const data = await response.json();
-          console.log("blogpost created", data);
-        } else {
-          const errorData = await response.json();
-          console.error("Error creating post :", errorData);
-        }
-      } catch (error) {
-        console.error("an error occurred: ", error);
+  async function createBlogpost(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://blog-api-production-c42d.up.railway.app/api/blogposts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.getItem("token")}`
+        },
+        body: JSON.stringify({
+          "title": title,
+          "text": text,
+          "topic": selectedTopic,
+        })
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("blogpost created", data);
+        navigate("/blogposts")
+      } else {
+        const errorData = await response.json();
+        console.error("Error creating post :", errorData);
       }
+    } catch (error) {
+      console.error("an error occurred: ", error);
     }
-  })
+  }
 
   if (!topics) {
     return <p>Loading ... </p>
@@ -65,15 +68,15 @@ export default function PostCreate() {
     <div>
       <h1>Create Post</h1>
 
-      <form>
+      <form onSubmit={createBlogpost}>
       <label htmlFor="title">Post title</label>
-        <input type="text" id="title" placeholder='Username' value={title} onChange={e => setTitle(e.target.value)} />
+        <input type="text" id="title" placeholder='title' value={title} onChange={e => setTitle(e.target.value)} />
         <label htmlFor="text">Text Title</label>
-        <input type="text" id="text" placeholder='Email' value={text} onChange={e => setText(e.target.value)} />
+        <input type="text" id="text" placeholder='your post' value={text} onChange={e => setText(e.target.value)} />
         <label htmlFor="topic">Topic</label>
         <select name="topic" id="topic">
           {topics.map((topic) => (
-            <option value={topic.title}>{topic.title}</option>
+            <option key={topic._id} value={topic._id}>{topic.title}</option>
           ))}
         </select>
         <input className='btn-submit' type="submit" value="Create Post" />
